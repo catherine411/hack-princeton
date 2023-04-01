9fe7beeac544ed4bd9fded77d44b27bd3bf433f8
Kim Hoang <kimberley.hoang528@gmail.com>
2023-04-01 18:54:40 -0400

    CORS error fix

--- backend/server.py
+++ backend/server.py
@@ -7,8 +7,9 @@ from werkzeug.utils import secure_filename
 from pytesseract import pytesseract
 import openai as openai
 import speech_recognition as sr
+from flask_cors import CORS, cross_origin
 
-openai.api_key = "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"
+openai.api_key = "sk-"
 # will have to install tesseract
 path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 UPLOAD_FOLDER = 'uploads'
@@ -19,11 +20,13 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 # Initializing flask app
 app = Flask(__name__)
+CORS(app)
+
 
 def summarize_transcript(input):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt= input + "\n\nTl;dr",
+        prompt=input + "\n\nTl;dr",
         temperature=0,
         max_tokens=200,
         top_p=1.0,
@@ -34,7 +37,7 @@ def summarize_transcript(input):
 
     return output
 
-    
+
 # Route for initialize the website
 @app.route("/")
 def index():
@@ -44,38 +47,44 @@ def index():
 # Receive uploaded .wav from html, convert to text
 # Converted text sent to GPT for summarization
 @app.route('/audio', methods=["GET", "POST"])
+@cross_origin()
 def get_audioSummary():
-
+    transcript = "testing"
     if request.method == "POST":
         print("Form Uploaded")
-
+        transcript = "post method??"
         if "audioFile" not in request.files:
+            transcript = "audioFile not in request.files"
             return redirect(request.url)
-        
+
         file = request.files["audioFile"]
         if file.filename == "":
+            transcript = "filename is empty string"
             return redirect(request.url)
 
         if file:
+            transcript = "there is a file"
             recognizer = sr.Recognizer()
             audioFile = sr.AudioFile(file)
             with audioFile as source:
                 data = recognizer.record(source)
             transcript = recognizer.recognize_google(data, key=None)
 
-        output = summarize_transcript(transcript)
+        # summary = summarize_transcript(transcript)
+        print(transcript)
+        # result = transcript + 'SEPARATIONSTRING' + summary
+    return transcript
 
-    return render_template('test.html', transcript=transcript, summary=output)
 
 @app.route('/text', methods=["GET", "POST"])
 def get_textSummary():
-
+    transcript = ""
     if request.method == "POST":
         print("Form Uploaded")
 
         if "textFile" not in request.files:
             return redirect(request.url)
-        
+
         file = request.files["textFile"]
         if file.filename == "":
             return redirect(request.url)
@@ -87,8 +96,7 @@ def get_textSummary():
 
         output = summarize_transcript(transcript)
 
-    return render_template('test.html', transcript = transcript, summary = output)
-
+    return render_template('test.html', transcript=transcript, summary=output)
 
 
 def allowed_file(filename):
@@ -143,13 +151,7 @@ def init():
 # for testing if it connects with react stuff
 
 
-
-
-
 #     return  render_template('test.html', transcript = transcript, summary = output)
-
-
-
 # Receive uploaded .wav from html, convert to text
 # Converted text sent to GPT for summarization
 # Running app
